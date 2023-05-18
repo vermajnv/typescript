ed4869a4ce95561abd337250625ab8286558051e
nayan_namespace <nayan.verma@gmail.com>
2023-05-18 15:44:59 +0530

    function with interface

--- build/js/fun.js
+++ build/js/fun.js
@@ -11,3 +11,14 @@ const getPerson = (person) => {
     return (_a = person.address) === null || _a === void 0 ? void 0 : _a.toLocaleLowerCase;
 };
 getPerson(person);
+const add = (a, b) => {
+    return a + b;
+};
+add(2, 4);
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg(add(5, 6));
+let multiply = (c, d) => {
+    return c * d;
+};
--- src/fun.ts
+++ src/fun.ts
@@ -28,3 +28,25 @@ const getPerson = (person: Person) =>
 }
 
 getPerson(person);
+
+
+const add = (a : number, b: number) : number | string => {
+    return a + b;
+}
+
+add(2, 4);
+
+const logMsg = (message : any) : void => {
+    console.log(message);
+}
+
+logMsg(add(5, 6))
+
+interface multType {
+    (a : number, b : number): number;
+} 
+
+let multiply:multType = (c, d) =>
+{
+    return c * d;
+}
