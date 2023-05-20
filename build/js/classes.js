9a82f91368587227c6ece39d840ec92955f74340
nayan_namespace <nayan.verma@gmail.com>
2023-05-20 11:30:17 +0530

    1st way to represent classes

---
+++ build/js/classes.js
@@ -0,0 +1,9 @@
+"use strict";
+class Coder {
+    constructor(name, age, lang, address) {
+        this.name = name;
+        this.age = age;
+        this.lang = lang;
+        this.address = address;
+    }
+}
---
+++ src/classes.ts
@@ -0,0 +1,20 @@
+class Coder {
+
+    name : string;
+    age : number;
+    lang : string;
+    address : string;
+    
+    constructor(
+        name : string,
+        age : number,
+        lang : string,
+        address : string 
+    )
+    {
+        this.name = name;
+        this.age = age;
+        this.lang = lang;
+        this.address = address;
+    }
+}
